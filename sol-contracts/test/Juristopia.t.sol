6c217f28a5aa22c1996c6265db0a4c4de5e0237b
timlucmiptev <info@timlucmiptev.space>
2024-06-22 16:44:10 +0200

    .

--- sol-contracts/test/Juristopia.t.sol
+++ sol-contracts/test/Juristopia.t.sol
@@ -19,7 +19,7 @@ test notes
 */
     function test_Exponent() public {
         UD60x18 n = convert(2);
-        UD60x18 e2 = juristopia.exponent(n);
+        UD60x18 e2 = n.exp();
         UD60x18 cost = e2.mul(convert(1 ether));
         console.log("e: %i", convert(e2));
         assertEq(convert(e2), 7);
