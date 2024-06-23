9ed2ce1e942f7e9fa81095d75239b9d1b19c7fb0
timlucmiptev <info@timlucmiptev.space>
2024-06-23 23:04:18 +0200

    tests

--- sol-contracts/test/Juristopia.t.sol
+++ sol-contracts/test/Juristopia.t.sol
@@ -103,4 +103,75 @@ contract JuristopiaTest is Test {
             "Density did not increment correctly"
         );
     }
+
+    function test_BadSpawnWorld() public {
+        Point memory testPoint = Point({x: 7, y: 8, z: 9});
+        string memory testName = "Test World";
+        string memory testDescription = "A test world description";
+
+        // Get the containing cube and its initial density
+        Point memory containingCube = juristopia.containingCube(testPoint);
+        bytes32 cubeCoord = juristopia.hashCoords(containingCube);
+        int32 initialDensity = juristopia.cubeCoordToDensity(cubeCoord);
+
+        // Calculate spawn cost
+        uint256 spawnCost = juristopia.spawnCost(
+            juristopia.pointDistance(testPoint, containingCube),
+            initialDensity
+        );
+
+        // Spawn the world
+        juristopia.spawnWorld{value: spawnCost}(
+            testPoint,
+            testName,
+            testDescription
+        );
+        // Attempt to spawn another world at the same coordinates
+        vm.expectRevert("World already exists");
+        juristopia.spawnWorld{value: spawnCost}(
+            testPoint,
+            "Another World",
+            "This should fail"
+        );
+
+        // Attempt to spawn a world with insufficient ETH
+        vm.expectRevert("Not enough ETH to spawn this world");
+        juristopia.spawnWorld{value: spawnCost - 1}(
+            Point({x: 10, y: 11, z: 12}),
+            "Underfunded World",
+            "This should also fail"
+        );
+
+        // Attempt to spawn a world with an empty name
+        vm.expectRevert("Name cannot be empty");
+        juristopia.spawnWorld{value: spawnCost}(
+            Point({x: 13, y: 14, z: 15}),
+            "",
+            "This should fail due to empty name"
+        );
+
+        // Attempt to spawn a world with an empty description
+        vm.expectRevert("Description cannot be empty");
+        juristopia.spawnWorld{value: spawnCost}(
+            Point({x: 16, y: 17, z: 18}),
+            "Valid Name",
+            ""
+        );
+
+        // Attempt to spawn a world with a name that's too long
+        vm.expectRevert("Name must be 32 characters or less");
+        juristopia.spawnWorld{value: spawnCost}(
+            Point({x: 19, y: 20, z: 21}),
+            "This name is way too long and should cause an error",
+            "Valid description"
+        );
+
+        // Attempt to spawn a world on a cube edge
+        vm.expectRevert("x is invalid: on cube edge");
+        juristopia.spawnWorld{value: spawnCost}(
+            Point({x: int128(cubeHalfSide * 2), y: 22, z: 23}),
+            "Edge World",
+            "This should fail due to being on a cube edge"
+        );
+    }
 }
